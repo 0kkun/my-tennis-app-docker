43e73884e339a53d9d8ddcd4770e4cc4e1a056e7
0kkun <shinji.okumoto.01@gmail.com>
2020-05-08 04:38:39 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   root to: 'top#index'
   resources :users
+  resource :sessions, only: [:new, :create, :destroy, :update]
+
+  resources :players do
+    member do #プレイヤーの一覧画面からお気に入り登録をする
+      post "add", to: "favorites#create"
+    end
+  end
+  #個人ページからお気に入りを削除する
+  resources :favorites, only: [:destroy]
+
+
 end
