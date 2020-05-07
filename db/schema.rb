e00d2dc74dab41fcd86e1fbc4b2397ae4652e6c0
0kkun <shinji.okumoto.01@gmail.com>
2020-05-08 01:22:50 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_154721) do
+ActiveRecord::Schema.define(version: 2020_05_07_161802) do
+
+  create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "player_id", null: false
+    t.index ["player_id"], name: "index_favorites_on_player_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "players", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
