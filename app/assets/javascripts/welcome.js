4ec72ddde2d2442a9d7b8822bf43631914a186ab
0kkun <shinji.okumoto.01@gmail.com>
2020-05-18 15:31:37 +0900

    View correct for notice message

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -3,6 +3,8 @@ $(function()  {
   $('h1.subtitle-text').fadeIn(2000);
   $('h2.discription').fadeIn(2000);
 
-  setTimeout("$('.notice').fadeOut('slow')", 1000) 
+  setTimeout("$('.notification').fadeOut('slow')", 1300) 
+
+  setTimeout("$('.missing').fadeOut('slow')", 1300) 
 
 });
