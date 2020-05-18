12f973920213590aa34ba6fc83557ef7710c2e49
0kkun <shinji.okumoto.01@gmail.com>
2020-05-18 15:34:22 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,13 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update]
 
   def show
-    @user = User.find(params[:id])
     #特定のユーザーが登録したお気に入りを全て取得する
     @favorites = Favorite.where("user_id = ?", @user)
     # @favorite_brand = BrandUser.where("user_id = ?", @user)
+    @players = Player.all
+
+    @brand_users = BrandUser.where("user_id = ?", @user)
+    @brands = Brand.all
   end
 
   def update
