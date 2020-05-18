ca3d4d1e7d9325220f47c65a3f02689244db0cf0
0kkun <shinji.okumoto.01@gmail.com>
2020-05-18 15:34:25 +0900

    Update favorites_controller.rb

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,7 +6,8 @@ class FavoritesController < ApplicationController
     @favorite = Favorite.new(player_id: @player_id, user_id: current_user.id)
     if @favorite.save
       #保存に成功した場合、選手一覧画面に戻る
-      redirect_to players_path, notice: '選手を登録しました'
+      # redirect_to players_path, notice: '選手を登録しました'
+      redirect_to user_path(current_user.id), notice: '選手を登録しました'
     end
   end
 
